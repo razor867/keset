52e030cd9c3ec65524ad57bdc7229edd9032b9bd
wap98 <wap@jiwalu.id>
2021-08-15 21:04:18 +0700

    add pages js

---
+++ public/js/pages/listdata_consumables.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+  const $thead = $("thead tr th").length;
+
+  $("#dataTable").DataTable({
+    processing: true,
+    oLanguage: {
+      sLengthMenu: "Tampilkan _MENU_ data per halaman",
+      sSearch: "Pencarian: ",
+      sZeroRecords: "Maaf, tidak ada data yang ditemukan",
+      sInfo: "Menampilkan _START_ s/d _END_ dari _TOTAL_ data",
+      sInfoEmpty: "Menampilkan 0 s/d 0 dari 0 data",
+      sInfoFiltered: "(di filter dari _MAX_ total data)",
+      // oPaginate: {
+      //   sFirst: "<<",
+      //   slast: ">>",
+      //   sPrevious: "<",
+      //   sNext: ">",
+      // },
+    },
+    language: {
+      search: "_INPUT_",
+      searchPlaceholder: "Search...",
+    },
+    columnDefs: [
+      {
+        orderable: false,
+        targets: $thead == 5 ? [] : [5],
+      },
+      {
+        className: "wrap-max-30",
+        targets: [0],
+      },
+      {
+        className: "wrap-max-10",
+        targets: [1, 3],
+      },
+      {
+        className: "wrap-max-20",
+        targets: [2, 4],
+      },
+      {
+        className: "wrap-max-10 dt-nowrap",
+        targets: [5],
+      },
+      {
+        //membuat format rupiah
+        render: $.fn.dataTable.render.number(",", ".", 2, "Rp. "),
+        targets: 4,
+      },
+    ],
+    // ordering: true,
+    // info: true,
+    serverSide: true,
+    responsive: true,
+    // stateSave: true,
+    scrollX: true,
+    ajax: {
+      url: base_url + "/assets/listdata_consumables",
+      type: "get",
+      error: function (e) {
+        console.log("data tidak ditemukan di server");
+      },
+      // success: function (data) {
+      //   console.log(data);
+      // },
+    },
+    columns: [
+      { data: "consumable_name", name: "consumable_name" },
+      { data: "type_name", name: "type_name" },
+      { data: "detail", name: "detail" },
+      { data: "qty", name: "qty" },
+      { data: "price", name: "price" },
+      { data: "action", name: "action" },
+    ],
+  });
+});
